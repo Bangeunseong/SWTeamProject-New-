7eb056d8fdd6af02b6502e6638ab40153878472e
Bangeunseong <bangeunseong@naver.com>
2022-11-07 16:18:02 +0900

    Bug Fix

--- TeamProjectFinal/SW설계기초-Final Project/PlayerInfo.h
+++ TeamProjectFinal/SW설계기초-Final Project/PlayerInfo.h
@@ -38,7 +38,7 @@ int PlayerPos = 0;
 int speed;
 
 //플레이어의 주 스킬, 보조 스킬, 사용 중인 스킬
-int CurrentSkill = 1, SubSkill = 2, UsingSkill = 0, SkillTime = 3; double SkillActivationTime = 0;
+int CurrentSkill = 0, SubSkill = 0, UsingSkill = 0, SkillTime = 3; double SkillActivationTime = 0;
 
 //플레이어의 스킬 셋 string
 char Skillstr[] = { "Skill1 -" }; char SubSkillstr[] = { "Skill2 -" };
