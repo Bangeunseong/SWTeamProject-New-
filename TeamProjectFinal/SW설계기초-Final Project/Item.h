743b14524cf37f1d6b0c4ca3b1d1d6e9250fdf9b
Bangeunseong <bangeunseong@naver.com>
2022-11-22 08:53:53 +0900

    Bug Fix

--- TeamProjectFinal/SW설계기초-Final Project/Item.h
+++ TeamProjectFinal/SW설계기초-Final Project/Item.h
@@ -10,7 +10,8 @@
 #ifndef ITEM_H
 #define ITEM_H
 
-//플레이어 출력 함수
+//----------------------------------------------------------------------------
+//------------------Movement of Player Using FlashItem-------------------
 void ShowFlashPlayer() {
 	if (Invinsible) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), PLAYERINVINSIBLEINDICATECOLOR);
 	else if (UsingSkill > 0) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), PLAYERUSINGSKILLCOLOR);
@@ -19,13 +20,13 @@ void ShowFlashPlayer() {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 	SetCurrentCursorPos(PLAYER_POS_X, PLAYER_POS_Y);
 }
-//플레이어 숨김 함수
+
 void HideFlashPlayer() {
 	for (int i = 0; i < 6; i++) { SetCurrentCursorPos(PLAYER_POS_X + i, PLAYER_POS_Y); printf(" "); }
 	SetCurrentCursorPos(PLAYER_POS_X, PLAYER_POS_Y);
 }
 
-//대쉬 스킬 사용 시 충돌 검사 함수
+
 int DetectCollision_FlashPlayerwithWall(int x, int y) {
 	for (int i = 0; i < 6; i++) {
 		if ((x + i < GAMEBOARD_ORIGIN_X + 2 || x + i >= GAMEBOARD_ORIGIN_X + GAMEBOARD_ROW) || (y <= GAMEBOARD_ORIGIN_Y || y >= GAMEBOARD_ORIGIN_Y + GAMEBOARD_COLUMN)) return 1;
@@ -33,7 +34,7 @@ int DetectCollision_FlashPlayerwithWall(int x, int y) {
 	return 0;
 }
 
-//대쉬 스킬 함수
+
 void flashUp() {
 	HideFlashPlayer();
 	if (!DetectCollision_FlashPlayerwithWall(PLAYER_POS_X, PLAYER_POS_Y - FLASHDISTANCE)) PLAYER_POS_Y -= FLASHDISTANCE;
@@ -59,12 +60,13 @@ void flashRight() {
 	ShowFlashPlayer();
 }
 
-//-------------------아이템별 작동함수-----------------------
+//---------------------------------------------------------------------------
+//------------------------Ability of each item------------------------------
 
-void itemSpeedUp() { CurSpeed += SPEEDINCREASERATE; }	//플레이어 캐릭터 속도 증가
-void itemBulletSpeedDown() { BulletSpeed /= 2.0; };					//탄막 속도 감소
-void itemInvinsibility() { Invinsible = 1; }										//무적 판정
-void itemFlash() {																		//대쉬 스킬
+void itemSpeedUp() { CurSpeed += SPEEDINCREASERATE; }	//Speed Increase to +0.5
+void itemBulletSpeedDown() { BulletSpeed /= 2.0; };					//BulletSpeed down to divided 2.0
+void itemInvinsibility() { Invinsible = 1; }										//Invinsible
+void itemFlash() {																		//Flash to direction
 	if (flashCount >= 2) return;
 	if (!flashFLAG) flashFLAG = 1;
 	if (GetAsyncKeyState(LEFT) & 0x8000) flashLeft();
@@ -73,7 +75,7 @@ void itemFlash() {																		//대쉬 스킬
 	if (GetAsyncKeyState(DOWN) & 0x8000) flashDown();
 	flashCount++; 
 };
-void itemDeleteBullet() {	//보드판에 존재하는 총알들 모두 삭제
+void itemDeleteBullet() {															//Delete all bullet on gameboard
 	for (int i = 0; i < BULLETCOUNT; i++) {
 		bullet[i].cnt = 0;
                 HideBullet(i);
@@ -85,11 +87,11 @@ void itemDeleteBullet() {	//보드판에 존재하는 총알들 모두 삭제
 	BULLETCOUNT = 0;
 };															
 
-//----------------------------------------------------------------
-//------------------아이템 인벤토리 관련 함수--------------------
+//-------------------------------------------------------------
+//------------------Item Status Functions--------------------
 
-//아이템 스킬 인벤토리 상태 확인
-int inventoryCheck() {
+
+int inventoryCheck() {	//Check the item inventory
 	if (SubSkill != 0) return 2;
 	else { 
 		if (CurSkill != 0) return 1;
@@ -97,8 +99,8 @@ int inventoryCheck() {
 	}
 }
 
-//아이템 스킬 교체 함수
-void SwapItem() {
+
+void SwapItem() {		//Swap item
 	if (inventoryCheck() == 2) {
 		HidePreviousCurrentNSubSkill();
 		int tmp = CurSkill;
@@ -108,13 +110,13 @@ void SwapItem() {
 	}
 }
 
-//아이템 습득함수
+//Getting item
 void getItem() { SubSkill = CurSkill; CurSkill = ItemNumber; }
 
-//---------------------------------------------------------------
-//------------------아이템 사용 관련 함수---------------------
+//------------------------------------------------------------------
+//-----------------Activating item skill functions------------------
 
-//아이템의 종류에 따라 작동하는 스킬
+//Activating Using skill
 void itemTrigger(int UsingSkill) {
 	switch (UsingSkill) {
 	case 1: itemSpeedUp();  break;
@@ -126,7 +128,7 @@ void itemTrigger(int UsingSkill) {
 	}
 }
 
-//아이템 스킬 해제 함수
+//Deactivate item(Invalidate Using Skill)
 void DeactivateSkillItem() { 
 	switch (UsingSkill) {
 	case 1: CurSpeed = SelectedSpeed; break;
@@ -139,18 +141,18 @@ void DeactivateSkillItem() {
 	UsingSkill = 0;
 }
 
-//아이템 스킬 발동 함수
+//Activate Item(Invalidate Current Skill)
 void ActivateSkillItem() {
 	if (UsingSkill != 0) { if (flashFLAG != 0) itemFlash(); return; }
 	if (!CurSkill) return;
-	HidePreviousCurrentNSubSkill();									//UI 갱신을 위해 넣은 숨김함수
-	UsingSkill = CurSkill; CurSkill = SubSkill; SubSkill = 0;	//주 스킬과 보조 스킬 Swap
-	itemTrigger(UsingSkill);													//스킬 발동
-	SkillActivationTime = TimeCheckerEnd() - PausingTime;						//스킬 발동 시간 기록
-	ShowCurrentNSubSkill();												//UI 갱신을 위해 넣은 출력함수
+	HidePreviousCurrentNSubSkill();									
+	UsingSkill = CurSkill; CurSkill = SubSkill; SubSkill = 0;	
+	itemTrigger(UsingSkill);													
+	SkillActivationTime = TimeCheckerEnd() - PausingTime;						
+	ShowCurrentNSubSkill();												
 }
 
-//아이템 스킬 지속시간 체크 함수
+//Checking Skill Activation Time
 void SkillTimeCheck() {
 	if (!UsingSkill) return;
 	if (TimeCheckerEnd() - PausingTime - SkillActivationTime > SkillTime) DeactivateSkillItem();
@@ -158,61 +160,61 @@ void SkillTimeCheck() {
 
 
 //----------------------------------------------------------------
-//-----------------아이템 무브먼트 관련 함수------------------
+//-----------------Item Creation Functions----------------------
 
-//아이템 움직임 딜레이 함수
+//Calculating Item Frame Buffer
 int CalculateItemTimeBuffer() {
 	if (TimeCheckerEnd() - PausingTime - ItemInputTime > ITEMTIMEBUFFER) { ItemInputTime += ITEMTIMEBUFFER; return 0; }
 	else return 1;
 }
 
-//아이템 삭제 함수
+//Hide item on board
 void HideItem() {
 	SetCurrentCursorPos(ITEM_POS_X, ITEM_POS_Y);
 	printf(" ");
 }
 
-//아이템 출력 함수
+//Show item on board
 void ShowItem() {
 	SetCurrentCursorPos(ITEM_POS_X, ITEM_POS_Y);
 	printf("%c", itemList[ItemNumber - 1]);
 }
 
-//아이템 충돌 검사 함수
+//DetectCollision with wall
 int DetectCollision_ItemwithWall(int x, int y) {
 	if (UniBoard[y - GAMEBOARD_ORIGIN_Y][x - GAMEBOARD_ORIGIN_X] == WALL) return 1;
 	return 0;
 }
 
-//아이템 생성 함수
+//Creating item in certain time
 void CreateItem() {
-	if ((TimeCheckerEnd() - PausingTime > ItemCreationLoop *(StageTime[StageNumber - 1]) / 3.0) && ItemCreationLoop < 3) {
-		ITEM_POS_X = rand() % (GAMEBOARD_ROW - 2) + GAMEBOARD_ORIGIN_X + 2;	//초기 랜덤 X좌표
-		ITEM_POS_Y = GAMEBOARD_ORIGIN_Y + 1;							//초기 Y좌표
-		ItemNumber = rand() % 5 + 1;													//아이템 넘버 랜덤 생성
-		ItemInputTime = TimeCheckerEnd() - PausingTime;					//아이템 생성 시간 기록
-		itemFLAG = 1;																			//게임 보드 내 아이템 아이콘 존재 유무 1
-		ShowItem();																				//아이템 출력
-		ItemCreationLoop++;																//현재까지 출력한 아이템 갯수
+	if ((TimeCheckerEnd() - PausingTime > ItemCreationLoop *(StageTime[StageNumber - 1]) / 3.0) && ItemCreationLoop < 3) {		//every one third of stagetime create item
+		ITEM_POS_X = rand() % (GAMEBOARD_ROW - 2) + GAMEBOARD_ORIGIN_X + 2;		//
+		ITEM_POS_Y = GAMEBOARD_ORIGIN_Y + 1;							
+		ItemNumber = rand() % 5 + 1;													
+		ItemInputTime = TimeCheckerEnd() - PausingTime;					
+		itemFLAG = 1;																			
+		ShowItem();																				
+		ItemCreationLoop++;																
 	}
 }
-
+//Creating item in certain time(for infinite mode)
 void CreateItemInInfiniteMode() {
 	if (TimeCheckerEnd() - PausingTime > 15.0 * ItemCreationLoop) {
-		ITEM_POS_X = rand() % (GAMEBOARD_ROW - 2) + GAMEBOARD_ORIGIN_X + 2;	//초기 랜덤 X좌표
-		ITEM_POS_Y = GAMEBOARD_ORIGIN_Y + 1;							//초기 Y좌표
-		ItemNumber = rand() % 5 + 1;													//아이템 넘버 랜덤 생성
-		ItemInputTime = TimeCheckerEnd() - PausingTime;					//아이템 생성 시간 기록
-		itemFLAG = 1;																			//게임 보드 내 아이템 아이콘 존재 유무 1
-		ShowItem();																				//아이템 출력
-		ItemCreationLoop++;																//현재까지 출력한 아이템 갯수
+		ITEM_POS_X = rand() % (GAMEBOARD_ROW - 2) + GAMEBOARD_ORIGIN_X + 2;	
+		ITEM_POS_Y = GAMEBOARD_ORIGIN_Y + 1;							
+		ItemNumber = rand() % 5 + 1;													
+		ItemInputTime = TimeCheckerEnd() - PausingTime;					
+		itemFLAG = 1;																			
+		ShowItem();																				
+		ItemCreationLoop++;																
 	}
 }
 
-//아이템 항목 초기화 함수
+//reset item
 void ResetItem() { HideItem(); itemFLAG = 0; }
 
-//아이템 좌표 1씩 감소시키는 함수
+//moving item down
 void shiftItemDown() {
 	HideItem();
 	if (ItemCollisionDetected) {
@@ -229,8 +231,8 @@ void shiftItemDown() {
 	ShowItem();
 }
 
-//-------------------------------------------------------------
-//-----------------아이템 갱신 함수(본체)--------------------
+//--------------------------------------------------------
+//-----------------Invalidating Item---------------------
 void InvalidateItem() {
 	if (!itemFLAG) { if (!GameMode) CreateItem(); else CreateItemInInfiniteMode(); }
 	else { 
