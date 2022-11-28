235218985cf4dc65fea6d11784af434762e96625
Bangeunseong <bangeunseong@naver.com>
2022-11-28 16:03:28 +0900

    사운드를 넣어야 하는데 도와주세요...잘모르겠어요

--- TeamProjectFinal/SW설계기초-Final Project/SoundPlay.h
+++ TeamProjectFinal/SW설계기초-Final Project/SoundPlay.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
+#pragma comment(lib, "winmm.lib")
 #include <Windows.h>
 #include <mmsystem.h>
-#pragma comment(lib, "winmm.lib")
 #include <Digitalv.h>
 #include "VariableSets.h"
 #include "Timer.h"
@@ -12,22 +12,21 @@ MCI_OPEN_PARMS openBgm;
 MCI_PLAY_PARMS playBgm;
 MCI_OPEN_PARMS openDamageSound;
 MCI_PLAY_PARMS playDamageSound;
-
-#define BGM "C:\\Users\\bange\\OneDrive\\바탕 화면\\Documents\\GitHub\\새 폴더\\TeamProjectFinal\\BGM.mp3"	//BGM 파일 확장명
-#define DAMAGE "C:\\사용자\\bange\\OneDrive\\바탕 화면\\Documents\\GitHub\\새 폴더\\TeamProjectFinal\\Explosion.mp3"		//DAMAGE 효과음 확장명
+#define BGM L"C:\\Users\\bange\\OneDrive\\바탕 화면\\Documents\\GitHub\\새 폴더\\TeamProjectFinal\\BGM.mp3"	//BGM 파일 확장명
+#define DAMAGE L"C:\\Users\\bange\\OneDrive\\바탕 화면\\Documents\\GitHub\\새 폴더\\TeamProjectFinal\\Explosion.mp3"		//DAMAGE 효과음 확장명
 //#define CLEAR ""		//게임 클리어 시 들어가는 Clear 효과음 확장명
-
 int dwID;
+
 void PlayBgm() {
 	openBgm.lpstrElementName = BGM;
-	openBgm.lpstrDeviceType = "mpegvideo";    //mp3 형식, wav 형식이면 "waveaudio" 작성
-	mciSendCommand(0, MCI_OPEN, MCI_OPEN_ELEMENT | MCI_OPEN_TYPE, (DWORD)(LPVOID)&openBgm);
+	openBgm.lpstrDeviceType = L"mpegvideo";    //mp3 형식, wav 형식이면 "waveaudio" 작성
+	mciSendCommand(NULL, MCI_OPEN, MCI_OPEN_ELEMENT | MCI_OPEN_TYPE, (DWORD)(LPVOID)&openBgm);
 	dwID = openBgm.wDeviceID;
 	mciSendCommand(dwID, MCI_PLAY, MCI_DGV_PLAY_REPEAT, (DWORD)(LPVOID)&playBgm);    //음악 반복 재생
 }
 void PlayDamageSound() {
 	openDamageSound.lpstrElementName = DAMAGE;    //파일 오픈
-	openDamageSound.lpstrDeviceType = "mpegvideo";    //mp3 형식
+	openDamageSound.lpstrDeviceType = L"mpegvideo";    //mp3 형식
 	mciSendCommand(0, MCI_OPEN, MCI_OPEN_ELEMENT | MCI_OPEN_TYPE, (DWORD)(LPVOID)&openDamageSound);
 	dwID = openDamageSound.wDeviceID;
 	mciSendCommand(dwID, MCI_PLAY, MCI_NOTIFY, (DWORD)(LPVOID)&openDamageSound);    //음악을 한 번 재생
